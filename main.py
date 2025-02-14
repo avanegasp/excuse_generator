1f68d0983c180fbd2ae858a5d2ac4648cdb7f2e7
avanegas <arco527@gmail.com>
2025-02-14 16:53:07 -0500

    Add code in main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello World")
+import random;
+
+who = ['My cat', 'The delivery guy', 'A ghost', 'My brother'];
+what = ['burned', 'dropped', 'spilled', 'lost'];
+when = ['before turning it in', 'right when I needed it', 'while I was sleeping', 'when I was in the bathroom', 'while I was doing something else'];
+
+excuse_created = random.choice(who) + " " + random.choice(what) + " " + random.choice(when) + ".";
+
+print(excuse_created)
